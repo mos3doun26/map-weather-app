73853507a90a45cea98ea3f42bf2f409e6aeff70
Mohamed Samir <mos3doun26@gmail.com>
2025-11-28 21:12:44 +0200

    Create fetching data func

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { useQuery } from "@tanstack/react-query"
 import AdditionalInfo from "./components/cards/AdditionalInfo"
 import CurrentWeather from "./components/cards/CurrentWeather"
 import DailyForecast from "./components/cards/DailyForecast"
 import HourlyForecast from "./components/cards/HourlyForecast"
+import { getWeather } from "../api"
 
 const App = () => {
+  const { data } = useQuery({
+    queryKey: ["weather"],
+    queryFn: () => getWeather({ lat: 50, lon: 50 })
+  })
+
   return (
     <main>
       <CurrentWeather />
