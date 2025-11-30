69a026d3fd786b42077bf41d240667052ede27dd
Mohamed Samir <mos3doun26@gmail.com>
2025-11-30 22:06:22 +0200

    Create add-info-card, Add svgs as react components using svgr-vite-plugin

--- src/components/cards/AdditionalInfo.jsx
+++ src/components/cards/AdditionalInfo.jsx
@@ -1,7 +1,68 @@
+import { useQuery } from "@tanstack/react-query"
+import Card from "./Card"
+import Cloud from "/src/assets/cloud.svg?react"
+import Sunrise from "/src/assets/sunrise.svg?react"
+import Sunset from "/src/assets/sunset.svg?react"
+import Wind from "/src/assets/wind.svg?react"
+import Uv from "/src/assets/uv.svg?react"
+import Pressure from "/src/assets/pressure.svg?react"
+import Humidity from "/src/assets/humidity.svg?react"
+import ArrowUp from "/src/assets/arrow-up.svg?react"
+
 const AdditionalInfo = () => {
+    const { data } = useQuery({
+        queryKey: ["weather"],
+        queryFn: () => getWeather({ lat: 50, lon: 50 })
+    })
+
+    const FormatedComponent = ({ value, number }) => {
+        if (value === "sunrise" || value === "sunset") {
+            return data?.timezone && data?.current?.dt ?
+                new Intl.DateTimeFormat("en-US", {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                    hour12: true,
+                    timeZone: data.timezone,
+                }).format(new Date(data.current.dt * 1000))
+                : "N/A"
+        }
+
+        if (value === "wind_deg") {
+            return <ArrowUp className="size-8 invert" style={{ transform: `rotate(${number}deg)` }} />
+        }
+
+        return number
+
+    }
+
     return (
-        <div>AdditionalInfo</div>
+        <Card title="Additional Weather Info" className="pt-4 flex flex-col gap-4">
+            {
+                rows.map(({ label, value, Icon }, index) =>
+                    <div key={index} className="flex justify-between items-center">
+                        <div className="flex gap-2 items-center">
+                            <span className="text-lg text-zinc-500 font-medium">{label}</span>
+                            <Icon className="size-8 invert" />
+                        </div>
+                        <span>
+                            <FormatedComponent value={value} number={data?.current[value]} />
+                        </span>
+                    </div>
+                )
+            }
+        </Card>
     )
 }
+
+
+const rows = [
+    { label: "Sunrise", value: "sunrise", Icon: Sunrise },
+    { label: "Sunset", value: "sunset", Icon: Sunset },
+    { label: "Pressure (hPa)", value: "pressure", Icon: Pressure },
+    { label: "Humidity", value: "humidity", Icon: Humidity },
+    { label: "UV Index", value: "uvi", Icon: Uv },
+    { label: "Cloudiness (%)", value: "clouds", Icon: Cloud },
+    { label: "Wind Direction", value: "wind_deg", Icon: Wind },
+];
 
 export default AdditionalInfo
