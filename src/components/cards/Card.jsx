21d6b80640ab3544ef43f00980926d743f35d23f
Mohamed Samir <mos3doun26@gmail.com>
2025-12-09 20:02:06 +0200

    Adding some animation for cards during loading

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -1,8 +1,12 @@
+import clsx from "clsx"
+
 const Card = ({ title, children, className }) => {
     return (
         <div className="p-4 bg-linear-to-br from-card to-card/60 rounded-xl shadow-md">
             <h1 className="text-3xl font-medium">{title}</h1>
-            <div className={className}>{children}</div>
+            <div
+                className={clsx(className, "animate-[fade-in_2s_ease-in-out_forwards]")}
+            >{children}</div>
         </div>
     )
 }
