1eb2db13e84862341d6ad1457e823a5b790daa45
Mohamed Samir <mos3doun26@gmail.com>
2025-12-11 20:40:30 +0200

    Some code enhancement in the side panel component

--- src/components/SidePanel.jsx
+++ src/components/SidePanel.jsx
@@ -25,33 +25,29 @@ function AirPollution({ coords }) {
             <span className="text-2xl font-bold">AQI</span>
             {
                 Object.entries(data?.list[0]?.components).map(([key, value]) => {
-                    const polluent = pollutantLevels[key]
+                    const pollutant = pollutantLevels[key]
 
                     const maxValue = (() => {
-                        const minMax = pollutantLevels[key]?.very_poor.min
-                        if (minMax) {
-                            return value > minMax ? value : minMax
-                        }
-                        return value > 1 ? value : 1
+                        const minMax = pollutant["Very Poor"].min
+                        return value > minMax ? value : minMax
                     })
 
                     const currentLevel = (() => {
                         switch (true) {
-                            case value >= polluent?.good.min && value <= polluent?.good.max:
+                            case value >= pollutant?.Good.min && value <= pollutant?.Good.max:
                                 return "Good";
-                            case value >= polluent?.fair.min && value <= polluent?.fair.max:
+                            case value >= pollutant?.Fair.min && value <= pollutant?.Fair.max:
                                 return "Fair";
-                            case value >= polluent?.moderate.min && value <= polluent?.moderate.max:
+                            case value >= pollutant?.Moderate.min && value <= pollutant?.Moderate.max:
                                 return "Moderate";
-                            case value >= polluent?.poor.min && value <= polluent?.poor.max:
+                            case value >= pollutant?.Poor.min && value <= pollutant?.Poor.max:
                                 return "Poor";
-                            case value >= polluent?.very_poor.min && value <= maxValue():
+                            case value >= pollutant["Very Poor"].min && value <= maxValue():
                                 return "Very Poor";
                             default:
                                 return "Very Poor";
                         }
                     })
-                    console.log(currentLevel())
 
                     return (
                         <div className="rounded-lg bg-linear-to-tr from-sidebar to-sidebar-accent p-2 flex flex-col gap-2">
@@ -59,9 +55,9 @@ function AirPollution({ coords }) {
                                 <span className="capitalize font-medium text-lg">{key}</span>
                                 <span className="font-medium text-lg">{value}</span>
                             </div>
-                            <Slider value={[value || 0]} max={maxValue()} disabled />
+                            <Slider value={[value]} max={maxValue()} disabled />
                             <div className="flex items-center justify-between">
-                                <span>{pollutantLevels[key]?.good.min || 0}</span>
+                                <span>{pollutant?.Good.min}</span>
                                 <span>{maxValue()}</span>
                             </div>
                             {/* levels */}
@@ -86,47 +82,61 @@ function AirPollution({ coords }) {
 
 const pollutantLevels = {
     so2: {
-        good: { min: 0, max: 20 },
-        fair: { min: 20, max: 80 },
-        moderate: { min: 80, max: 250 },
-        poor: { min: 250, max: 350 },
-        very_poor: { min: 350, max: null }
+        Good: { min: 0, max: 20 },
+        Fair: { min: 20, max: 80 },
+        Moderate: { min: 80, max: 250 },
+        Poor: { min: 250, max: 350 },
+        "Very Poor": { min: 350, max: null }
     },
     no2: {
-        good: { min: 0, max: 40 },
-        fair: { min: 40, max: 70 },
-        moderate: { min: 70, max: 150 },
-        poor: { min: 150, max: 200 },
-        very_poor: { min: 200, max: null }
+        Good: { min: 0, max: 40 },
+        Fair: { min: 40, max: 70 },
+        Moderate: { min: 70, max: 150 },
+        Poor: { min: 150, max: 200 },
+        "Very Poor": { min: 200, max: null }
     },
     pm10: {
-        good: { min: 0, max: 20 },
-        fair: { min: 20, max: 50 },
-        moderate: { min: 50, max: 100 },
-        poor: { min: 100, max: 200 },
-        very_poor: { min: 200, max: null }
+        Good: { min: 0, max: 20 },
+        Fair: { min: 20, max: 50 },
+        Moderate: { min: 50, max: 100 },
+        Poor: { min: 100, max: 200 },
+        "Very Poor": { min: 200, max: null }
     },
     pm2_5: {
-        good: { min: 0, max: 10 },
-        fair: { min: 10, max: 25 },
-        moderate: { min: 25, max: 50 },
-        poor: { min: 50, max: 75 },
-        very_poor: { min: 75, max: null }
+        Good: { min: 0, max: 10 },
+        Fair: { min: 10, max: 25 },
+        Moderate: { min: 25, max: 50 },
+        Poor: { min: 50, max: 75 },
+        "Very Poor": { min: 75, max: null }
     },
     o3: {
-        good: { min: 0, max: 60 },
-        fair: { min: 60, max: 100 },
-        moderate: { min: 100, max: 140 },
-        poor: { min: 140, max: 180 },
-        very_poor: { min: 180, max: null }
+        Good: { min: 0, max: 60 },
+        Fair: { min: 60, max: 100 },
+        Moderate: { min: 100, max: 140 },
+        Poor: { min: 140, max: 180 },
+        "Very Poor": { min: 180, max: null }
     },
     co: {
-        good: { min: 0, max: 4400 },
-        fair: { min: 4400, max: 9400 },
-        moderate: { min: 9400, max: 12400 },
-        poor: { min: 12400, max: 15400 },
-        very_poor: { min: 15400, max: null }
-    }
+        Good: { min: 0, max: 4400 },
+        Fair: { min: 4400, max: 9400 },
+        Moderate: { min: 9400, max: 12400 },
+        Poor: { min: 12400, max: 15400 },
+        "Very Poor": { min: 15400, max: null }
+    },
+    no: {
+        Good: { min: 0, max: 20 },
+        Fair: { min: 20, max: 40 },
+        Moderate: { min: 40, max: 60 },
+        Poor: { min: 60, max: 80 },
+        "Very Poor": { min: 80, max: null },
+    },
+    nh3: {
+        Good: { min: 0, max: 40 },
+        Fair: { min: 40, max: 70 },
+        Moderate: { min: 70, max: 150 },
+        Poor: { min: 150, max: 200 },
+        "Very Poor": { min: 200, max: null },
+    },
 };
 
 const levels = ["Good", "Fair", "Moderate", "Poor", "Very Poor"]
